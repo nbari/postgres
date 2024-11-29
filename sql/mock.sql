9f47740776eeda4b8594a0884c76984403c20734
nbari <nbari@tequila.io>
2024-11-29 09:01:36 +0100

    cleanup sql/mock

--- sql/mock.sql
+++ sql/mock.sql
@@ -1,24 +1,28 @@
+-- Connect to the mock database using psql:
 -- psql -h localhost -U postgres -d mock -f mock.sql
 
--- create postgres database
+-- Create the mock database (if it doesn't already exist)
 CREATE DATABASE mock;
 
--- create table
+-- Switch to the mock database (execute this only if you're not already connected)
+\c mock;
+
+-- Create the sms table
 DROP TABLE IF EXISTS sms;
 CREATE TABLE sms (
- id serial PRIMARY KEY,
- sms text NOT NULL,
- cdate timestamptz
+    id serial PRIMARY KEY,
+    sms text NOT NULL,
+    cdate timestamptz
 );
 
-
+-- Insert sample data using generate_series
 INSERT INTO sms (sms, cdate)
 SELECT
-    'Foo ' || EXTRACT(MINUTE FROM time_hour) AS name,
-    time_hour
+    'Foo ' || EXTRACT(MINUTE FROM time_hour) AS sms,
+    time_hour AS cdate
 FROM
     generate_series(
         NOW() - INTERVAL '5 minutes',
         NOW(),
-        INTERVAL '1 minute'
-    ) as time_hour;
+        INTERVAL '1 minute'            -- Step interval
+    ) AS time_series(time_hour);
